755b0501777444dc1f7e2eb40717c4d1645e9890
yongyuanlin <76577177+yongyuanlin@users.noreply.github.com>
2021-06-29 16:29:33 +0800

    Update jd_jszjb.js

--- jd_jszjb.js
+++ jd_jszjb.js
@@ -16,7 +16,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
-let InviterPin = ''; //
+let InviterPin = '3YHXsVaRTbL2weEuVM76cA=='; //
 
 
 if (process.env.InviterPin) {
