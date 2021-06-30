d15a5d832e45506e7c28233a8bee749772a3b56b
yongyuanlin <76577177+yongyuanlin@users.noreply.github.com>
2021-06-30 16:33:53 +0800

    Update jd_europeancup.js

--- jd_europeancup.js
+++ jd_europeancup.js
@@ -31,7 +31,7 @@ if ($.isNode()) {
 const JD_API_HOST = `https://api.m.jd.com/client.action`;
 message = ""
 $.actid = "901100032442101"
-$.shareuuid = "" //俺的助力码 
+$.shareuuid = "66af661979a54522b894450b37dc942f" //俺的助力码 
     !(async () => {
         if (!cookiesArr[0]) {
             $.msg($.name, '【提示】请先获取cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {
